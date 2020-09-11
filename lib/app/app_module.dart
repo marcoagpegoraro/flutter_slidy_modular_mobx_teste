85e9862fd78d45f30114f2cf216b0ac9ae055800
Marco Antonio <marco.ag.pegoraro@hotmail.com>
2020-09-10 23:04:55 -0300

    Recriado o projeto utilizando o slidy

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,19 +1,20 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_modular_teste/app/modules/other/other_page.dart';
+
+import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_modular_teste/app/app_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_modular_teste/app/app_widget.dart';
 import 'package:flutter_modular_teste/app/modules/home/home_module.dart';
-import 'package:flutter_modular_teste/app/modules/other/other_page.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => AppController()),
+        $AppController,
       ];
 
   @override
   List<ModularRouter> get routers => [
-        ModularRouter('/', module: HomeModule()),
+        ModularRouter(Modular.initialRoute, module: HomeModule()),
         ModularRouter('/other', child: (_, args) => OtherPage()),
         // ModularRouter('/other/:text',
         //     child: (_, args) => OtherPage(text: args.params['text']))
@@ -21,4 +22,6 @@ class AppModule extends MainModule {
 
   @override
   Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_teste/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  runApp(ModularApp(module: AppModule()));
-}
+void main() => runApp(ModularApp(module: AppModule()));
